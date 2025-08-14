4729dc9f2692f802522cb353c41ea22732188156
ARTIT G <rtiix8@gmail.com>
2025-08-14 11:40:27 +0700

    #

--- tests/test_backup_mysql.py
+++ tests/test_backup_mysql.py
@@ -1,6 +1,6 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# import sys
+# import os
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
 from unittest.mock import patch, MagicMock
@@ -11,7 +11,7 @@ class TestBackupMySQL(unittest.TestCase):
     @patch("backup_mysql.ensure_dir")
     @patch("backup_mysql.cleanup_old_files")
     @patch("subprocess.run")
-    @patch("backup_mysql.Queue")  # patch ถูกต้องตาม namespace ของ backup_mysql
+    # @patch("backup_mysql.Queue")  # patch ถูกต้องตาม namespace ของ backup_mysql
     def test_backup_mysql_runs(self, mock_queue_cls, mock_run, mock_cleanup, mock_ensure):
         mock_queue = MagicMock()
         mock_queue_cls.return_value = mock_queue
--- tests/test_backup_sqlserver.py
+++ tests/test_backup_sqlserver.py
@@ -1,6 +1,6 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# import sys
+# import os
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
 from unittest.mock import patch, MagicMock
@@ -11,7 +11,7 @@ class TestBackupSQLServer(unittest.TestCase):
     @patch("backup_sqlserver.ensure_dir")
     @patch("backup_sqlserver.cleanup_old_files")
     @patch("subprocess.run")
-    @patch("backup_sqlserver.Queue")  # patch class จริง
+    # @patch("backup_sqlserver.Queue")  # patch class จริง
     def test_backup_sqlserver_runs(self, mock_queue_cls, mock_run, mock_cleanup, mock_ensure):
         mock_queue = MagicMock()
         mock_queue_cls.return_value = mock_queue
