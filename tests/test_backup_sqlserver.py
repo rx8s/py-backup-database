ced2a620ccb8e4acda20c9a0cfc976a1231c22c7
ARTIT G <rtiix8@gmail.com>
2025-08-14 11:29:37 +0700

    sys path

--- tests/test_backup_sqlserver.py
+++ tests/test_backup_sqlserver.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 from unittest.mock import patch, MagicMock
 from backup_sqlserver import backup_sqlserver
